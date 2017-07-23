cec1f10002117e4ee1f51c8f438362b71bf25447
Jordi Barrero <jordibar@gmail.com>
2017-07-23 23:54:48 +0200

    Definimos la tarea de compilacinnn de JS

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,14 @@ var sass = require("gulp-sass"); //Importamos gulp-sass, es el puente entre gulp
 var notify = require("gulp-notify");
 var browserSync = require("browser-sync").create();
 var gulpImport = require("gulp-html-import");
+var tap = require("gulp-tap");
+var browserify = require("browserify");
+var buffer = require("gulp-buffer");
 
 // Definimos la tarea por defecto
 //decimos donde tiene que buscar archivos sass, carpeta y subcarpetas
 //cuando hay cambios ejecuta la tarea 'sass'
-gulp.task("default", ["html", "sass"], function() {
+gulp.task("default", ["html", "sass", "js"], function() {
 
     //Iniciamos el servidor de desarrollo en la carpeta src
     browserSync.init({ server: "dist/" });
@@ -15,9 +18,12 @@ gulp.task("default", ["html", "sass"], function() {
     //Observa los cambios en los archivos SASS, y entonces ejecutamos la tarea 'sass'
     gulp.watch(["./src/scss/*.scss", "./src/scss/**/*.scss"], ["sass"]);
 
-    //Observa los cambios en los archivos html, y recarga el navegador
+    //Observa los cambios en los archivos html, y recarga el navegador ejecutando la tarea 'html'
     //También hemos incluido la carpeta de components
     gulp.watch(["./src/*.html", "./src/**/*.html"], ["html"]);
+
+    //Observa cambios en los archivos JS y entonces compila el JS de nuevo ejcutando la tarea 'js'
+    gulp.watch(["./src/js/*.js", "./src/js/**/*.js]"], ["js"]);
 });
 
 
@@ -32,10 +38,31 @@ gulp.task("sass", function(){
 });
 
 
+
 // Definimos la tarea html para copiar e importar html
 gulp.task("html", function() {
     gulp.src("src/*.html") //Coge todos los html de la carpeta src
         .pipe(gulpImport("src/components/")) //carpeta donde estaran los trozos de html que va a poder importar
         .pipe(gulp.dest("dist/")) //carpeta donde deja las copias
         .pipe(browserSync.stream()) // Recargamos el navegador
+});
+
+
+
+
+// Compilar y generar un único JavaScript
+gulp.task("js", function() {
+    gulp.src('src/js/main.js')
+        .pipe(tap(function(file) { // tap nos permite ejecutar una función por cada fichero selecionado en gulp.src
+            //reemplazamos el contenido del fichero por lo que nos devuelve browserify pasándole el fichero
+            file.contents = browserify(file.path)  //creamos una instancia de browserify en base al archivo
+                            .transform("babelify", {presets: ["es2015"]}) //traduce nuestro código de ES6 a ES5
+                            .bundle() //compilamos el archivo
+                            .on("error", function(error){ //En caso de error mostramos una notificación
+                                return notify().write(error);
+                            });
+        }))
+        .pipe(buffer())  // convertimos a buffer para que funcione el siguiente pipe
+        .pipe(gulp.dest("dist/")) // lo guardamos en la carpeta dist
+        .pipe(browserSync.stream()); // recargamos el navegador
 });
---
+++ src/js/main.js
@@ -0,0 +1,3 @@
+const name = "Jordi Barrero";
+
+console.log(`Hello ${name}`);
