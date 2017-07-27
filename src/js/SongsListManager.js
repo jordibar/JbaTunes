3624a0a152021770cc51de52db4972bf101719f4
Jordi Barrero <jordibar@gmail.com>
2017-07-27 18:59:29 +0200

    seguimos refactorizando, ahora en la rama alternative, pendiente del merge con master

--- src/js/SongsListManager.js
+++ src/js/SongsListManager.js
@@ -13,8 +13,43 @@ export default class SongsListManager {
 
     }
 
+
     loadSongs() {
         // Aquí va el código que teníamos en main.js de carga de canciones
         //con algunas modificaciones para que funcione
+        this.songsService.list(songs => {
+            //Comprovamos si hay canciones
+            if(songs.lenght == 0) {
+                //Mostramos el estado vacío
+                this.uiManager.setEmpty();
+            }else{
+                //Llamamos al método que pinta el html con las canciones
+                this.renderSongs(songs);
+
+                //Quitamos el mensaje de cargando y ponemos la clase ideal
+                this.uiManager.setIdeal();
+            }
+        }, error => {
+            //Mostrar el estado de error
+            this.uiManager.setError();
+            //Hacemos log del error en la consola
+            console.log("ERROR", error);
+        })
+    }
+
+    renderSongs(songs) {
+        //Componemos el HTML con todas las canciones
+        let html = "";
+        for (let song of songs) {
+            //Template strings de ES6
+            //Componemos el html con todas las canciones
+            html += `<article class="song">
+                        <img src="${song.cover_url}" alt="${song.artist} - ${song.title}" class="cover">
+                        <div class="artist">${song.artist}</div>
+                        <div class="title">${song.title}</div>
+                    </article>`;
+        }
+        //Metemos el HTML en el div que contiene las canciones
+        $(".songs-list .ui-state.ideal").html(html);
     }
 }
--- src/js/main.js
+++ src/js/main.js
@@ -2,82 +2,15 @@
 window.$ = window.jQuery = require("jquery");  // Muy importante para utilizar jQuery con otros frameworks como Bootstrap
 
 import SongsService from "./SongsService";
+import UIManager from "./UIManager";
+import SongsListManager from "./SongsListManager.js"
 
 //Instanciamos SongService
-const songsService = new SongsService();
+const songsService = new SongsService("/songs/");
+const songListUIManager = new UIManager(".songs-list");
 
-
-
-// Cargar la lista de canciones con AJAX
-songsService.list(songs => {
-    //Comprovamos si hay canciones
-    if(songs.lenght == 0) {
-        //Mostramos el estado vacío
-        $(".songs-list").removeClass("loading").addClass("empty");
-    }else{
-        //Componemos el HTML con todas las canciones
-        let html = "";
-        for (let song of songs) {
-            //Template strings de ES6
-            //Componemos el html con todas las canciones
-            html += `<article class="song">
-                        <img src="${song.cover_url}" alt="${song.artist} - ${song.title}" class="cover">
-                        <div class="artist">${song.artist}</div>
-                        <div class="title">${song.title}</div>
-                    </article>`;
-        }
-                //Metemos el HTML en el div que contiene las canciones
-                $(".songs-list .ui-state.ideal").html(html);
-
-                //Quitamos el mensaje de cargando y ponemos la clase ideal
-                $(".songs-list").removeClass("loading").addClass("ideal");
-    }
-}, error => {
-    //Mostrar el estado de error
-    $(".songs-list").removeClass("loading").addClass("error");
-    //Hacemos log del error en la consola
-    console.log("ERROR", error);
-})
-
-
-/*
-$.ajax({
-    url: "/songs/",
-    success: songs => {
-        console.log("SUCCESS", songs);
-
-        //Comprovamos si hay canciones
-        if(songs.lenght == 0) {
-            //Mostramos el estado vacío
-            $(".songs-list").removeClass("loading").addClass("empty");
-        }else{
-            //Componemos el HTML con todas las canciones
-            let html = "";
-            for (let song of songs) {
-                //Template strings de ES6
-                //Componemos el html con todas las canciones
-                html += `<article class="song">
-                            <img src="${song.cover_url}" alt="${song.artist} - ${song.title}" class="cover">
-                            <div class="artist">${song.artist}</div>
-                            <div class="title">${song.title}</div>
-                        </article>`;
-            }
-                    //Metemos el HTML en el div que contiene las canciones
-                    $(".songs-list .ui-state.ideal").html(html);
-
-                    //Quitamos el mensaje de cargando y ponemos la clase ideal
-                    $(".songs-list").removeClass("loading").addClass("ideal");
-        }
-    },
-
-    error: error => {
-        //Mostrar el estado de error
-        $(".songs-list").removeClass("loading").addClass("error");
-        //Hacemos log del error en la consola
-        console.log("ERROR", error);
-    }
-});
-*/
+const songsListManager = new SongsListManager(songsService, songListUIManager); // Inyección de dependencias
+songsListManager.init(); //inicializamos SongsListManager que es donde está la carga de canciones
 
 
 
